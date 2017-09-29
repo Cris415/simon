8443d9884e21ff93da77f4eabceb7c16020400af
Cristian Reynoso <cris@Cristians-MacBook-Pro.local>
2017-09-29 12:24:01 -0400

    Add final style to control panel, and functioning power button

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-var gameBtns = document.getElementById("gameBtns");
-gameBtns.addEventListener("click", tileClick);
-var buttons = ["g", "r", "y", "b"];
+const gameBtns = document.getElementById("gameBtns");
+const buttons = ["g", "r", "y", "b"];
 var sequence = [];
 var userSequence = [];
+var count = 0;
+var simonOnOff = false;
 
 function tileClick(event) {
   var clickedBtn = event.target.id;
@@ -12,12 +13,14 @@ function tileClick(event) {
   //Check if sequences do not match, show the last sequence again
   if (!compareArrays(userSequence, sequence, false)) {
     userSequence = [];
+    playSequence();
     return console.log(sequence);
   }
 
   if (compareArrays(userSequence, sequence, true)) {
     userSequence = [];
     addToSequence();
+    playSequence();
   }
 }
 
@@ -37,13 +40,6 @@ function compareArrays(arr1, arr2, fllChk) {
   return true;
 }
 
-//Lights up chosen button
-// function toggleBtn(button) {
-//   //untoggle .5sec later
-//   document.getElementById(button).classList.toggle("btnOn");
-//   //setTimeout(document.getElementById(button).classList.toggle("btnOn"), 0.3);
-// }
-
 //Random number fn from MDN
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -60,10 +56,66 @@ function addToSequence() {
 
 //Start the game
 function init() {
+  gameBtns.disabled = false;
+  gameBtns.addEventListener("click", tileClick);
+  simonOnOff = true;
+  restart(false);
   addToSequence();
+  playSequence();
+}
+function shutOff() {
+  simonOnOff = false;
+  restart(false);
+  //Remove "light on" class, so the device stops blinking when off
+  // for (let i = 0; i < buttons.length; i++) {
+  //   document.getElementById(buttons[i]).classList.remove(buttons[i] + "On");
+  // }
+  gameBtns.disabled = true;
+  gameBtns.removeEventListener("click", tileClick);
 }
 
-init();
+function restart(shut) {
+  if (simonOnOff) {
+    sequence = [];
+    userSequence = [];
+    count = 0;
+    if (shut) {
+      console.log("called init");
+      init();
+    }
+  }
+}
+
+document.getElementById("onOff").addEventListener("click", onOff);
+function onOff() {
+  if (simonOnOff) {
+    shutOff();
+    document.getElementById("onOff").classList.toggle("onButton");
+    simonOnOff = false;
+  } else {
+    init();
+    document.getElementById("onOff").classList.toggle("onButton");
+    simonOnOff = true;
+  }
+}
+
+document.getElementById("restart").addEventListener("click", restart);
+
+function playSequence() {
+  for (let i = 0; i < sequence.length; i++) {
+    setTimeout(function() {
+      lightButton(sequence[i]);
+    }, 1500 * i);
+  }
+  setTimeout(function() {}, 2000 * sequence.length);
+}
+
+function lightButton(id) {
+  document.getElementById(id).classList.toggle(id + "On");
+  setTimeout(function() {
+    document.getElementById(id).classList.toggle(id + "On");
+  }, 1000);
+}
 // var array1 = [1, 2, 5, 7];
 // var array2 = [1, 2, 5, 7];
 // var array3 = [1, 2, 5, 7, 8];
