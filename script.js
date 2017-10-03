c10b6f249fe40bdc11e959f4df7f3ab00e7fe541
Cristian Reynoso <cris@Cristians-MacBook-Pro.local>
2017-10-03 14:22:55 -0400

    Refactor and organize JS

--- script.js
+++ script.js
@@ -1,195 +1,195 @@
+//Simon Game By Cristian Sept 2017
 const gameBtns = document.getElementById("gameBtns");
 const display = document.getElementById("display");
 const buttons = ["g", "r", "y", "b"];
-var sequence = [];
-var userSequence = [];
-var count = 1;
-var simonOnOff = false;
-var simonStrict = false;
-const vicSteps = 20;
 
-//True will pause, false will play.
-function playAudio(btn, bool) {
-  const audioLib = {
-    r: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
-    g: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
-    b: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
-    y: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3")
-  };
-  bool = bool || false;
-
-  if (!bool) {
-    audioLib[btn].play();
-  } else {
-    audioLib[btn].pause();
-  }
-}
-function pauseFn(event) {
-  var clickedBtn = event.target.id;
-  playAudio(clickedBtn, true);
-}
-function tileClick(event) {
-  var clickedBtn = event.target.id;
-  playAudio(clickedBtn);
-  //If user presses outside of the buttons but within the click event area just outside of the curved edges
-  if (clickedBtn.length === 0) {
-    return;
-  }
-  //Add the button to sequence
-  userSequence.push(clickedBtn);
-
-  console.log(userSequence, "user");
-
-  //Check if sequences do not match, show the last sequence again
-  if (!compareArrays(userSequence, sequence, false)) {
-    if (!simonStrict) {
-      userSequence = [];
-      playSequence();
-      return console.log(sequence);
+var audio = {
+  toggle: function(btn, play) {
+    const audioLib = {
+      r: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
+      g: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+      b: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
+      y: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3")
+    };
+    if (play) {
+      audioLib[btn].play();
     } else {
-      //If strict game will restart
-      //Add display msg "lose or X"
-      display.innerHTML = "X";
-      //reset game
-      setTimeout(function() {
-        restart(true);
-      }, 1000);
+      audioLib[btn].pause();
     }
+  },
+  pauseToggle: function(event) {
+    var clickedBtn = event.target.id;
+    audio.toggle(clickedBtn, false);
   }
+};
 
-  if (compareArrays(userSequence, sequence, true)) {
-    if (sequence.length === vicSteps) {
-      //Add display msg "win or W "
-      display.innerHTML = "W";
-      //reset game
-      setTimeout(function() {
-        restart(true);
-      }, 1000);
-    } else {
-      userSequence = [];
-      addToSequence();
-      count = sequence.length;
-      display.innerHTML = count;
-      setTimeout(function() {
-        playSequence();
-      }, 1500);
-    }
-  }
-}
-
-//Will return true if arrays match, false if not
-function compareArrays(arr1, arr2, fllChk) {
-  if (fllChk) {
-    if (arr1.length !== arr2.length) {
-      return false;
-    }
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-//Random number fn from MDN
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
-
-//Adds a random button to the sequence
-function addToSequence() {
-  var randNum = getRandomInt(0, 4);
-  sequence.push(buttons[randNum]);
-  console.log(sequence);
-}
-
-//Start the game
-function init() {
-  gameBtns.disabled = false;
-  document.getElementById("restart").addEventListener("click", restart);
-  document.getElementById("strict").addEventListener("click", simonStrictFn);
-  gameBtns.addEventListener("mousedown", tileClick);
-  gameBtns.addEventListener("mouseup", pauseFn);
-  simonOnOff = true;
-  restart(false);
-  addToSequence();
-  playSequence();
-}
-function shutOff() {
-  simonOnOff = false;
-  display.innerHTML = "";
-  restart(false);
-  // Remove "light on" class, so the device stops blinking when off
-  // for (let i = 0; i < buttons.length; i++) {
-  //   document.getElementById(buttons[i]).classList.remove(buttons[i] + "On");
-  // }
-
-  for (let i = 0; i < buttons.length; i++) {
-    playAudio(buttons[i], true);
-  }
-  gameBtns.removeEventListener("mousedown", tileClick);
-  gameBtns.removeEventListener("mouseup", pauseFn);
-}
-
-function restart(shut) {
-  if (simonOnOff) {
-    sequence = [];
-    userSequence = [];
-    count = 1;
-    display.innerHTML = count;
-    if (shut) {
-      init();
-    }
-  }
-}
-
-function simonStrictFn() {
-  if (simonOnOff) {
-    if (simonStrict) {
-      document.getElementById("strict").classList.toggle("strictOn");
-      simonStrict = false;
-    } else {
-      document.getElementById("strict").classList.toggle("strictOn");
-      simonStrict = true;
-    }
-  }
-}
-
-document.getElementById("onOff").addEventListener("click", onOff);
-function onOff() {
-  if (simonOnOff) {
-    shutOff();
-    document.getElementById("onOff").classList.toggle("onButton");
-    simonOnOff = false;
-  } else {
-    init();
-    document.getElementById("onOff").classList.toggle("onButton");
-    simonOnOff = true;
-  }
-}
-
-//Lights up buttons based on sequence array
-function playSequence() {
-  for (let i = 0; i < sequence.length; i++) {
-    setTimeout(function() {
-      if (simonOnOff) {
-        playAudio(sequence[i]);
-        lightButton(sequence[i]);
+var util = {
+  compareArrays: function(arr1, arr2, fllChk) {
+    //fllChk true will compare array length, to get full comparison
+    if (fllChk) {
+      if (arr1.length !== arr2.length) {
+        return false;
       }
-    }, 1500 * i);
+    }
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  },
+  getRandomInt: function(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
   }
-}
-
-//Lights up the chosen button
-function lightButton(id) {
-  if (simonOnOff) {
-    document.getElementById(id).classList.toggle(id + "On");
-    setTimeout(function() {
+};
+var simon = {
+  state: {
+    sequence: [],
+    userSequence: [],
+    count: 1,
+    simonOn: false,
+    strict: false
+  },
+  checkSequence: function(check) {
+    const stepsToWin = 20;
+    switch (check) {
+      case "partial":
+        if (
+          !util.compareArrays(
+            simon.state.userSequence,
+            simon.state.sequence,
+            false
+          )
+        ) {
+          if (!simon.state.strict) {
+            simon.state.userSequence = [];
+            display.innerHTML = "X";
+            simon.playSequence();
+          } else {
+            //If strict game will restart
+            display.innerHTML = "XX";
+            setTimeout(function() {
+              simon.reset(true);
+            }, 2000);
+          }
+        }
+        break;
+      case "full":
+        //If user has entered correct sequence, add another step or declare win if reached vicStep
+        if (
+          util.compareArrays(
+            simon.state.userSequence,
+            simon.state.sequence,
+            true
+          )
+        ) {
+          if (simon.state.sequence.length === stepsToWin) {
+            display.innerHTML = "W";
+            setTimeout(function() {
+              reset(true);
+            }, 1800);
+          } else {
+            simon.state.userSequence = [];
+            simon.addToSequence();
+            simon.playSequence();
+          }
+        }
+        break;
+    }
+  },
+  addToSequence: function() {
+    let randNum = util.getRandomInt(0, 4);
+    simon.state.count = simon.state.sequence.length + 1;
+    display.innerHTML = simon.state.count;
+    simon.state.sequence.push(buttons[randNum]);
+  },
+  lightButton: function(id) {
+    if (simon.state.simonOn) {
       document.getElementById(id).classList.toggle(id + "On");
-    }, 1000);
+      setTimeout(function() {
+        document.getElementById(id).classList.toggle(id + "On");
+      }, 1000);
+    }
+  },
+  playSequence: function() {
+    setTimeout(function() {
+      display.innerHTML = simon.state.count;
+      for (let i = 0; i < simon.state.sequence.length; i++) {
+        setTimeout(function() {
+          if (simon.state.simonOn) {
+            audio.toggle(simon.state.sequence[i], true);
+            simon.lightButton(simon.state.sequence[i]);
+          }
+        }, 1500 * i);
+      }
+    }, 1500);
+  },
+  startGame: function() {
+    gameBtns.disabled = false;
+    document.getElementById("restart").addEventListener("click", simon.reset);
+    document
+      .getElementById("strict")
+      .addEventListener("click", simon.simonStrictToggle);
+    gameBtns.addEventListener("mousedown", simon.clickOnGameButton);
+    gameBtns.addEventListener("mouseup", audio.pauseToggle);
+    simon.state.simonOn = true;
+    simon.reset(false);
+    simon.addToSequence();
+    simon.playSequence();
+  },
+  turnOff: function() {
+    simon.state.simonOn = false;
+    display.innerHTML = "";
+    simon.reset(false);
+    gameBtns.removeEventListener("mousedown", simon.clickOnGameButton);
+    gameBtns.removeEventListener("mouseup", audio.pauseToggle);
+  },
+  reset: function(restart) {
+    if (simon.state.simonOn) {
+      simon.state.sequence = [];
+      simon.state.userSequence = [];
+      simon.state.count = 1;
+      display.innerHTML = simon.state.count;
+      if (restart) {
+        simon.startGame();
+      }
+    }
+  },
+  simonStrictToggle: function() {
+    if (simon.state.simonOn) {
+      if (simon.state.strict) {
+        document.getElementById("strict").classList.toggle("strictOn");
+        simon.state.strict = false;
+      } else {
+        document.getElementById("strict").classList.toggle("strictOn");
+        simon.state.strict = true;
+      }
+    }
+  },
+  powerToggle: function() {
+    if (simon.state.simonOn) {
+      simon.turnOff();
+      document.getElementById("onOff").classList.toggle("onButton");
+      simon.state.simonOn = false;
+    } else {
+      simon.startGame();
+      document.getElementById("onOff").classList.toggle("onButton");
+      simon.state.simonOn = true;
+    }
+  },
+  clickOnGameButton: function(event) {
+    var clickedBtn = event.target.id;
+    //Ignore outside clicks
+    if (clickedBtn.length === 0) {
+      return;
+    }
+    audio.toggle(clickedBtn, true);
+    simon.state.userSequence.push(clickedBtn);
+    simon.checkSequence("partial");
+    simon.checkSequence("full");
   }
-}
+};
+
+document.getElementById("onOff").addEventListener("click", simon.powerToggle);
